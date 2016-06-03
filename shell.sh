d35347ffe721340798b0db877576ef9f708d4f8d
navy <navyxie2010@gmail.com>
2016-06-03 11:52:40 +0800

    add array end string plus

--- shell.sh
+++ shell.sh
@@ -11,3 +11,19 @@ echo "pid:"$$
 echo "params:"$@
 echo "params length:"$#
 echo "code:"$?
+
+#数组赋值
+
+array=('a' 'b' 'c' 'd')
+
+#数组遍历
+
+for item in ${array[@]}
+do
+    echo $item
+done
+
+#字符串拼接
+str='abc'
+str=${str}"def"
+echo $str # abcdef
